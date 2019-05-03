bf3c24c75f10f7bdc2c9f8d43c54d962c2809a40
Ellen (Yufei) Chen <chenellen007@gmail.com>
2019-05-03 13:44:58 -0400

    Made changes

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Typography from '@material-ui/core/Typography/Typography';
+import Typography from "@material-ui/core/Typography/Typography";
 
 import "./intro.css";
 
 const intro = props => {
     return (
         <div id="intro">
-            <Typography variant="h5" component="h5">
-          This is the intro!
-        </Typography>
+            <Typography variant="h2" component="h2">
+                This page is still under construction! Feel free to follow the
+                progress right here.
+            </Typography>
         </div>
     );
 };
