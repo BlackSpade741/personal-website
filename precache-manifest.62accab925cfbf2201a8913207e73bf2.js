5e7482d9aaf846507b6c5bd378153730b5fbc7b9
Ellen (Yufei) Chen <chenellen007@gmail.com>
2020-07-26 18:32:05 -0700

    Updated files

---
+++ precache-manifest.62accab925cfbf2201a8913207e73bf2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c343212f4ea15b0a4b14d4de6e77eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44f73aa33550bb2c19ee",
+    "url": "/static/css/main.4b37b22f.chunk.css"
+  },
+  {
+    "revision": "3dca236653e31def6b32",
+    "url": "/static/js/2.62e99b27.chunk.js"
+  },
+  {
+    "revision": "97ebbf6dad4d2821019aa12cf9d09c89",
+    "url": "/static/js/2.62e99b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f73aa33550bb2c19ee",
+    "url": "/static/js/main.0e4ad85f.chunk.js"
+  },
+  {
+    "revision": "2cad4a73b4cc6b8cecc2",
+    "url": "/static/js/runtime-main.b9ce08cb.js"
+  },
+  {
+    "revision": "b9c25239a49db20c2d2084bb19876132",
+    "url": "/static/media/Myfont-Regular.b9c25239.otf"
+  },
+  {
+    "revision": "2ca122acea65d99937933f36a5cf6a3e",
+    "url": "/static/media/ellen-pic.2ca122ac.jpg"
+  },
+  {
+    "revision": "d39cc7498671abc89bb8d22c955255fc",
+    "url": "/static/media/headshot.d39cc749.png"
+  },
+  {
+    "revision": "1f26879ae8373cadc9d645190f98d01c",
+    "url": "/static/media/myfont-regular-webfont.1f26879a.svg"
+  },
+  {
+    "revision": "d60af04870f3ec2be7159ed3ab5c520f",
+    "url": "/static/media/myfont-regular-webfont.d60af048.woff"
+  },
+  {
+    "revision": "f121731b6e5b078300f3cab78a8c2ffd",
+    "url": "/static/media/myfont-regular-webfont.f121731b.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+  "/precache-manifest.62accab925cfbf2201a8913207e73bf2.js"
 );
 
-importScripts("/precache-manifest.207cb916a11942e25b7b7f6c625644d1.js");
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
-workbox.clientsClaim();
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,9 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
-    blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
